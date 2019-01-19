2807e6e9573af015532c71b4e8279c2a5be1ad49
Josh Turner <joshaturner1@googlemail.com>
2019-01-19 23:53:26 +0000

    Starting image reading

--- ambilight.py
+++ ambilight.py
@@ -1,8 +1,8 @@
-# import subprocess
+import subprocess
 import random
 from PIL import Image
 
-# subprocess.call('./scrot.sh')
+subprocess.call('./scrot.sh')
 
 im = Image.open('screenshots/img.png') # Can be many different formats.
 pix = im.load()
@@ -12,15 +12,15 @@ totalg = 0
 totalb = 0
 
 for i in range(10):
-    x = random.randint(1, 48+1)
-    y = random.randint(1, 48+1)
+    x = random.randint(48, 48+48+1)
+    y = random.randint(0, 48)
     print(x, ", ", y, ": ", pix[x, y])
     totalr += pix[x, y][0]
     totalg += pix[x, y][1]
     totalb += pix[x, y][2]
 
-totalr /= 10
-totalg /= 10
-totalb /= 10
+totalr //= 10
+totalg //= 10
+totalb //= 10
 
 print(totalr, totalg, totalb)
