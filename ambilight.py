0c790ed8a3db1332f2c8e762ff6bb249242a519e
Josh Turner <joshaturner1@googlemail.com>
2019-01-20 13:46:58 +0000

    Image reading is done

--- ambilight.py
+++ ambilight.py
@@ -1,26 +1,90 @@
 import subprocess
 import random
+import time
 from PIL import Image
 
-subprocess.call('./scrot.sh')
+PIXELS_TO_PROCESS = 100
+PIXELS_PER_LED = 80
+HORIZONTAL_PIXEL_GAP = 80
+VERTICAL_PIXEL_GAP = 120
+HORIZONTAL_PIXEL_COUNT = 2560
+VERTICAL_PIXEL_COUNT = 1440
 
-im = Image.open('screenshots/img.png') # Can be many different formats.
-pix = im.load()
+leftLEDArray = [(0, 0, 0)] * 15
+topLEDArray = [(0, 0, 0)] * 30
+rightLEDArray = [(0, 0, 0)] * 15
 
-totalr = 0
-totalg = 0
-totalb = 0
+start = time.time()
 
-for i in range(10):
-    x = random.randint(48, 48+48+1)
-    y = random.randint(0, 48)
-    print(x, ", ", y, ": ", pix[x, y])
-    totalr += pix[x, y][0]
-    totalg += pix[x, y][1]
-    totalb += pix[x, y][2]
+while True:
+    subprocess.call('./scrot.sh')
 
-totalr //= 10
-totalg //= 10
-totalb //= 10
+    im = Image.open('screenshots/img.png')
+    pix = im.load()
 
-print(totalr, totalg, totalb)
+    # FILLING IN THE TOP LEDS
+
+    totalR = 0
+    totalG = 0
+    totalB = 0
+
+    for i in range(30):
+        for j in range(PIXELS_TO_PROCESS):
+            x = random.randint(HORIZONTAL_PIXEL_GAP + i * PIXELS_PER_LED, HORIZONTAL_PIXEL_GAP + (i+1) * PIXELS_PER_LED)
+            y = random.randint(0, PIXELS_PER_LED)
+            totalR += pix[x, y][0]
+            totalG += pix[x, y][1]
+            totalB += pix[x, y][2]
+
+        totalR //= 10
+        totalG //= 10
+        totalB //= 10
+
+        topLEDArray[i] = (totalR, totalG, totalB)
+
+    # FILLING IN THE LEFT LEDS
+
+    totalR = 0
+    totalG = 0
+    totalB = 0
+
+    for i in range(15):
+        for j in range(PIXELS_TO_PROCESS):
+            x = random.randint(0, PIXELS_PER_LED)
+            y = random.randint(HORIZONTAL_PIXEL_GAP + i * PIXELS_PER_LED, HORIZONTAL_PIXEL_GAP + (i+1) * PIXELS_PER_LED)
+            totalR += pix[x, y][0]
+            totalG += pix[x, y][1]
+            totalB += pix[x, y][2]
+
+        totalR //= 10
+        totalG //= 10
+        totalB //= 10
+
+        leftLEDArray[i] = (totalR, totalG, totalB)
+
+    leftLEDArray.reverse()
+
+    # FILLING IN THE RIGHT LEDS
+
+    totalR = 0
+    totalG = 0
+    totalB = 0
+
+    for i in range(15):
+        for j in range(PIXELS_TO_PROCESS):
+            x = random.randint(HORIZONTAL_PIXEL_COUNT - PIXELS_PER_LED, HORIZONTAL_PIXEL_COUNT - 1)
+            y = random.randint(HORIZONTAL_PIXEL_GAP + i * PIXELS_PER_LED, HORIZONTAL_PIXEL_GAP + (i+1) * PIXELS_PER_LED)
+            totalR += pix[x, y][0]
+            totalG += pix[x, y][1]
+            totalB += pix[x, y][2]
+
+        totalR //= 10
+        totalG //= 10
+        totalB //= 10
+
+        rightLEDArray[i] = (totalR, totalG, totalB)
+
+    end = time.time()
+    break
+
+print(end - start)
