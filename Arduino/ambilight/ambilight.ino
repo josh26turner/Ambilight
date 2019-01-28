f00afb0dee160aac2fe8d84bc9a6622e765509c1
Josh Turner <joshaturner1@googlemail.com>
2019-01-28 16:35:51 +0000

    Serial Event function

--- Arduino/ambilight/ambilight.ino
+++ Arduino/ambilight/ambilight.ino
@@ -7,24 +7,31 @@ CRGB leds[NUM_LEDS];
 void setup() { 
   Serial.begin(9600);
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
-  for (int i = 0; i < NUM_LEDS - 2; i ++){
+  for (int i = 0; i < 15; i ++){
+    leds[i] = CRGB(0,0,255);
+  }
+  for (int i = 15; i < 45; i ++){
     leds[i] = CRGB(255,0,0);
+  }
+  for (int i = 45; i < 60; i ++){
+    leds[i] = CRGB(0,255,0);
   }
   FastLED.show();
 }
  
 void loop() { 
-  if (Serial.available() > 0){
-    for (int i = 0; i < NUM_LEDS; i ++){
-      if (Serial.available() > 0){
-        char r = Serial.read();
-        char g = Serial.read();
-        char b = Serial.read();
-        int index = i - 2;
-        if (index < 0) index += NUM_LEDS;
-        if (index < NUM_LEDS - 2) leds[index] = CRGB(r, g, b);
-        FastLED.show();
-      }
+}
+
+void serialEvent() {
+  for (int i = 0; i < NUM_LEDS; i ++){
+    if (Serial.available() > 0){
+      char r = Serial.read();
+      char g = Serial.read();
+      char b = Serial.read();
+      int index = i - 2;
+      if (index < 0) index += NUM_LEDS;
+      if (index < NUM_LEDS - 2) leds[index] = CRGB(r, g, b);
+      FastLED.show();
     }
   }
 }
