a3fc3eb4ba1a47b83b82c3cd531c2a605e47994b
Josh Turner <joshaturner1@googlemail.com>
2019-02-15 14:45:05 +0000

    C-files

---
+++ C/cmake-build-debug/CMakeFiles/C.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C.dir/main.c.o"
+  "C.pdb"
+  "C"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
