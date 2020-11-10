de4b634aac99bef361a9b78a5729e07e2e27b8f1
Josh Turner <joshaturner1@googlemail.com>
2020-11-10 22:02:50 +0000

    Enable background to stay

--- C/background.c
+++ C/background.c
@@ -66,7 +66,7 @@ int set_interface_attribs (int fd, int speed, int parity) {
 	tty.c_cflag &= ~(PARENB | PARODD);      // shut off parity
 	tty.c_cflag |= parity;
 	tty.c_cflag &= ~CSTOPB;
-	//tty.c_cflag &= ~CRTSCTS;
+	tty.c_cflag &= ~HUPCL;
 
 	if (tcsetattr (fd, TCSANOW, &tty) != 0) {
 		//error_message ("error %d from tcsetattr", errno);
